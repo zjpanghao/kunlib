811c98d42d65a189e2a31a0d0b9c86883c681966
zjpanghao <zjpanghao@gmail.com>
2019-11-18 17:20:41 +0800

    add timeout

--- user/cpost/cpost.cpp
+++ user/cpost/cpost.cpp
@@ -9,9 +9,7 @@ CPost::CPost() {
 }
 
 static size_t myWriteData(void *buffer, size_t size, size_t nmemb, void *userp) {
-  LOG(INFO) << "recv:" << size << " nm:" << nmemb;
   std::string *result = (std::string *)userp;
-  LOG(INFO) << "userp" << result;
   result->append((char*)buffer, 0, 
       size *nmemb); 
   return size * nmemb;
@@ -24,16 +22,14 @@ size_t CPost::writeData(void *buffer, size_t size, size_t nmemb, void *userp) {
 }
 
 int CPost::post(const std::string &url, const std::string &body, std::string &result) {
-  LOG(INFO) << "RESULT:" << &result;
   CURL *curl = curl_easy_init();
   if (!curl) {
     return -1;
   }
   curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-  //curl_easy_setopt(curl, CURLOPT_POST, 1);
   struct curl_slist *list = NULL;
   list = curl_slist_append(list, "Content-Type:application/json;");
-  //curl_easy_setopt(curl, CURLOPT_HTTPHEADER, url.c_str());
+  curl_easy_setopt(curl, CURLOPT_TIMEOUT, 20L);
   curl_easy_setopt(curl, CURLOPT_COPYPOSTFIELDS, body.c_str());
   std::function<void(void *, size_t, size_t, void*)>func = std::bind(&CPost::writeData, this, std::placeholders::_1, 
       std::placeholders::_2, 
@@ -42,13 +38,13 @@ int CPost::post(const std::string &url, const std::string &body, std::string &re
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, myWriteData);
   curl_easy_setopt(curl, CURLOPT_WRITEDATA,&result);
   int res = curl_easy_perform(curl);
-  curl_slist_free_all(list);
   /* Check for errors */
   if(res != CURLE_OK){
-    curl_easy_cleanup(curl);
-    return res;
+    goto DONE;
   }
+DONE:
+  curl_slist_free_all(list);
   curl_easy_cleanup(curl);
-  return 0;
+  return res;
 }
 }
