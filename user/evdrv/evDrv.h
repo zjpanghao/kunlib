3ef3529ea854dfcb8186b0b183b5c990a2420ed2
zjpanghao <panghao@localhost.localdomain>
2020-01-02 10:53:36 +0800

    move private

--- user/evdrv/evDrv.h
+++ user/evdrv/evDrv.h
@@ -29,24 +29,28 @@ class EvDrv {
        const kunyan::Config &confg,
        const std::vector<std::shared_ptr<GeneralControl>> &controls);
    virtual ~EvDrv() = default;
+  static std::string getBufferStr(struct evbuffer *buf);
+  static bool getBufferJson(struct evbuffer *buf, Json::Value &root);
+
+ void process(EvHttpRequest *req, 
+     const HttpControl *control);
+ private:
+  virtual bool getQueryJson(EvHttpRequest*req, Json::Value &root) = 0;
    virtual int evReqMethod(EvHttpRequest*req) = 0;
    virtual evbuffer *getInputBuffer(EvHttpRequest *req) = 0;
+
+   virtual void sendResponseBody(
+       EvHttpRequest *req,
+       const std::string &value) = 0;
    virtual void sendResponse(EvHttpRequest *req,
        const Json::Value &value) = 0;
-   virtual void sendResponseBody(EvHttpRequest *req,
-       const std::string &value) = 0;
+
    virtual void start(
        const std::string &ip,
        int port, 
        int nThread, 
        const std::vector<std::shared_ptr<GeneralControl>> &controls) = 0;
-  static std::string getBufferStr(struct evbuffer *buf);
-  static bool getBufferJson(struct evbuffer *buf, Json::Value &root);
 
-  virtual bool getQueryJson(EvHttpRequest*req, Json::Value &root) = 0;
-
- void process(EvHttpRequest *req, 
-     const HttpControl *control);
 };
 
 #endif
